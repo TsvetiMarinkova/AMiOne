c65172abc4a42bc1569fa68336aaa6759b856355
TsvetiMarinkova <tsvetimarinkova28@gmail.com>
2025-10-20 20:22:06 +0100

    test - Remove lazy loading from GalleryItem image

--- src/components/GalleryItem.jsx
+++ src/components/GalleryItem.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function GalleryItem({ item }) {
   return (
     <div className="gallery-item position-relative overflow-hidden rounded">
-      <img src={item.image} loading="lazy" alt={item.title} />
+      <img src={item.image} alt={item.title} />
       <div className="gallery-overlay position-absolute top-0 start-0 w-100 h-100 d-flex flex-column justify-content-end p-3">
         <div>
           <h6 className="mb-0">{item.title}</h6>
